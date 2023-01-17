2d7d7932b1eddff6c71dae936f7ea40a770d0282
shashkist <shashkist@gmail.com>
2023-01-17 13:22:23 +0200

    3d

--- src/main/java/com/ilia/tacos/HomeController.java
+++ src/main/java/com/ilia/tacos/HomeController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.GetMapping;
     public class HomeController {
         @GetMapping("/")
         public String home() {
-            System.out.println("rry4353r");
+
 
             return "home";
         }
